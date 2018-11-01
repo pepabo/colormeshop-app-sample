7b2a100f35d66cfd2cc8509fc705d8234c0ef267
yinm <yinm@pepabo.com>
2018-11-01 09:17:15 +0900

    認可のキャンセル時の画面を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :colormeshop, ENV['COLORMESHOP_CLIENT_ID'], ENV['COLORMESHOP_CLIENT_SECRET'], { scope: 'read_script_tags write_script_tags' }
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
